51e3d4be33ce1d052ed7bfac7953530f4c28d04a
codergon <atakerewilliams@gmail.com>
2025-01-29 11:24:17 +0100

    Initial Commit

---
+++ PushNotifications/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PushNotifications
+//
+//  Created by Kester Atakere on 31/01/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PushNotifications/PushNotificationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PushNotificationsApp.swift
+//  PushNotifications
+//
+//  Created by Kester Atakere on 31/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct PushNotificationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
